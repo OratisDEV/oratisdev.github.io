ed08450c9cb3706b089a2a88d52aaa209077c70b
Kuco <85517097+KucoDEV@users.noreply.github.com>
2024-05-16 20:52:28 +0200

    Updated

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
   var typed = new Typed('.typing', {
     strings: [
       "Keyscape",
-      "an original bot",
+      "un bot original!",
     ],
     typeSpeed: 130,
     backSpeed: 90,
