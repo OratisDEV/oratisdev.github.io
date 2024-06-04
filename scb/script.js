0e315e6ef3ff3939f938c716c899da84fe32d7b0
Kuco <85517097+KucoDEV@users.noreply.github.com>
2024-06-04 20:25:56 +0200

    Updated SCBuild-ER

--- scb/script.js
+++ scb/script.js
@@ -35,19 +35,101 @@ document.addEventListener('DOMContentLoaded', (event) => {
         clone.addEventListener('dragstart', dragStartOnMap);
         clone.addEventListener('dragend', dragEnd);
         clone.addEventListener('contextmenu', removeBuilding);
+        
+        // Police
+        if (clone.dataset.type === 'building' && originalElement.id === 'improved-police-station') {
+            clone.addEventListener('mouseenter', show24xHighlightZone);
+            clone.addEventListener('mouseleave', remove24xHighlightZone);
+        }
 
-        if (clone.dataset.type === 'building' && originalElement.id === 'hospital') {
-            clone.addEventListener('mouseenter', show24xHighlightZone);
-            clone.addEventListener('mouseleave', remove24xHighlightZone);
+        else if (clone.dataset.type === 'building' && originalElement.id === 'police-station') {
+            clone.addEventListener('mouseenter', show12xHighlightZone);
+            clone.addEventListener('mouseleave', remove12xHighlightZone);
+        
         }
-        else if (clone.dataset.type === 'building' && originalElement.id === 'improved-police-station') {
-            clone.addEventListener('mouseenter', show24xHighlightZone);
-            clone.addEventListener('mouseleave', remove24xHighlightZone);
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'small-police-station') {
+            clone.addEventListener('mouseenter', show6x8HighlightZone);
+            clone.addEventListener('mouseleave', remove6x8HighlightZone);
         }
+
+        // Fire
         else if (clone.dataset.type === 'building' && originalElement.id === 'improved-fire-station') {
             clone.addEventListener('mouseenter', show22xHighlightZone);
             clone.addEventListener('mouseleave', remove22xHighlightZone);
         }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'fire-station') {
+            clone.addEventListener('mouseenter', show10x12HighlightZone);
+            clone.addEventListener('mouseleave', remove10x12HighlightZone);
+        }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'small-fire-station') {
+            clone.addEventListener('mouseenter', show6x8HighlightZone);
+            clone.addEventListener('mouseleave', remove6x8HighlightZone);
+        }
+
+        // Health
+        else if (clone.dataset.type === 'building' && originalElement.id === 'hospital') {
+            clone.addEventListener('mouseenter', show24xHighlightZone);
+            clone.addEventListener('mouseleave', remove24xHighlightZone);
+        }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'clinic') {
+            clone.addEventListener('mouseenter', show12xHighlightZone);
+            clone.addEventListener('mouseleave', remove12xHighlightZone);
+        }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'small-clinic') {
+            clone.addEventListener('mouseenter', show8xHighlightZone);
+            clone.addEventListener('mouseleave', remove8xHighlightZone);
+        }
+
+        // ----------------------------- Services ----------------------------- \\
+
+        // Electricity
+        else if (clone.dataset.type === 'building' && originalElement.id === 'nuclear-plant') {
+            clone.addEventListener('mouseenter', show12xRHighlightZone);
+            clone.addEventListener('mouseleave', remove12xRHighlightZone);
+        }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'oil-power-plant') {
+            clone.addEventListener('mouseenter', show8xRHighlightZone);
+            clone.addEventListener('mouseleave', remove8xRHighlightZone);
+        }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'coal-fired-power-plant') {
+            clone.addEventListener('mouseenter', show10xHighlightZone);
+            clone.addEventListener('mouseleave', remove10xHighlightZone);
+        }
+
+        // Waste Water
+        else if (clone.dataset.type === 'building' && originalElement.id === 'drain-pipe') {
+            clone.addEventListener('mouseenter', show12xRHighlightZone);
+            clone.addEventListener('mouseleave', remove12xRHighlightZone);
+        }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'small-drain-pipe') {
+            clone.addEventListener('mouseenter', show10xHighlightZone);
+            clone.addEventListener('mouseleave', remove10xHighlightZone);
+        }
+
+        // Trash
+        else if (clone.dataset.type === 'building' && originalElement.id === 'waste-incinerator') {
+            clone.addEventListener('mouseenter', show16xHighlightZone);
+            clone.addEventListener('mouseleave', remove16xHighlightZone);
+        }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'dump') {
+            clone.addEventListener('mouseenter', show12xRHighlightZone);
+            clone.addEventListener('mouseleave', remove12xRHighlightZone);
+        }
+
+        else if (clone.dataset.type === 'building' && originalElement.id === 'small-dump') {
+            clone.addEventListener('mouseenter', show10xHighlightZone);
+            clone.addEventListener('mouseleave', remove10xHighlightZone);
+        }
+
     }
 
     function dragStartOnMap(e) {
@@ -208,7 +290,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         highlightZone.style.position = 'absolute';
         highlightZone.style.width = `${24 * gridCellSize}px`;
         highlightZone.style.height = `${24 * gridCellSize}px`;
-        highlightZone.style.backgroundColor = 'rgba(0, 255, 0, 0.5)';
+        highlightZone.style.backgroundColor = 'rgba(0, 255, 0, 0.4)';
         highlightZone.style.pointerEvents = 'none';
         highlightZone.style.zIndex = '9999';
         highlightZone.id = 'highlight-zone';
@@ -237,7 +319,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         highlightZone.style.position = 'absolute';
         highlightZone.style.width = `${22 * gridCellSize}px`;
         highlightZone.style.height = `${22 * gridCellSize}px`;
-        highlightZone.style.backgroundColor = 'rgba(0, 255, 0, 0.5)';
+        highlightZone.style.backgroundColor = 'rgba(0, 255, 0, 0.4)';
         highlightZone.style.pointerEvents = 'none';
         highlightZone.style.zIndex = '9999';
         highlightZone.id = 'highlight-zone';
@@ -257,4 +339,237 @@ document.addEventListener('DOMContentLoaded', (event) => {
             highlightZone.remove();
         }
     }
+
+    function show12xHighlightZone(e) {
+        const target = e.target;
+        const gridCellSize = map.getBoundingClientRect().width / 50;
+        const highlightZone = document.createElement('div');
+    
+        highlightZone.style.position = 'absolute';
+        highlightZone.style.width = `${12 * gridCellSize}px`;
+        highlightZone.style.height = `${12 * gridCellSize}px`;
+        highlightZone.style.backgroundColor = 'rgba(0, 255, 0, 0.4)';
+        highlightZone.style.pointerEvents = 'none';
+        highlightZone.style.zIndex = '9999';
+        highlightZone.id = 'highlight-zone';
+    
+        const left = parseFloat(target.style.left);
+        const top = parseFloat(target.style.top);
+    
+        highlightZone.style.left = `${left - ((12 - parseFloat(target.dataset.width)) / 2) * gridCellSize}px`;
+        highlightZone.style.top = `${top - ((12 - parseFloat(target.dataset.height)) / 2) * gridCellSize}px`;
+    
+        document.getElementById('overlay').appendChild(highlightZone);
+    }
+    
+    function remove12xHighlightZone() {
+        const highlightZone = document.getElementById('highlight-zone');
+        if (highlightZone) {
+            highlightZone.remove();
+        }
+    }
+
+    function show12xRHighlightZone(e) {
+        const target = e.target;
+        const gridCellSize = map.getBoundingClientRect().width / 50;
+        const highlightZone = document.createElement('div');
+    
+        highlightZone.style.position = 'absolute';
+        highlightZone.style.width = `${12 * gridCellSize}px`;
+        highlightZone.style.height = `${12 * gridCellSize}px`;
+        highlightZone.style.backgroundColor = 'rgba(255, 0, 0, 0.4)';
+        highlightZone.style.pointerEvents = 'none';
+        highlightZone.style.zIndex = '9999';
+        highlightZone.id = 'highlight-zone';
+    
+        const left = parseFloat(target.style.left);
+        const top = parseFloat(target.style.top);
+    
+        highlightZone.style.left = `${left - ((12 - parseFloat(target.dataset.width)) / 2) * gridCellSize}px`;
+        highlightZone.style.top = `${top - ((12 - parseFloat(target.dataset.height)) / 2) * gridCellSize}px`;
+    
+        document.getElementById('overlay').appendChild(highlightZone);
+    }
+    
+    function remove12xRHighlightZone() {
+        const highlightZone = document.getElementById('highlight-zone');
+        if (highlightZone) {
+            highlightZone.remove();
+        }
+    }
+    
+    function show8xHighlightZone(e) {
+        const target = e.target;
+        const gridCellSize = map.getBoundingClientRect().width / 50;
+        const highlightZone = document.createElement('div');
+    
+        highlightZone.style.position = 'absolute';
+        highlightZone.style.width = `${8 * gridCellSize}px`;
+        highlightZone.style.height = `${8 * gridCellSize}px`;
+        highlightZone.style.backgroundColor = 'rgba(0, 255, 0, 0.4)';
+        highlightZone.style.pointerEvents = 'none';
+        highlightZone.style.zIndex = '9999';
+        highlightZone.id = 'highlight-zone';
+    
+        const left = parseFloat(target.style.left);
+        const top = parseFloat(target.style.top);
+    
+        highlightZone.style.left = `${left - ((8 - parseFloat(target.dataset.width)) / 2) * gridCellSize}px`;
+        highlightZone.style.top = `${top - ((8 - parseFloat(target.dataset.height)) / 2) * gridCellSize}px`;
+    
+        document.getElementById('overlay').appendChild(highlightZone);
+    }
+    
+    function remove8xHighlightZone() {
+        const highlightZone = document.getElementById('highlight-zone');
+        if (highlightZone) {
+            highlightZone.remove();
+        }
+    }
+    
+    function show8xRHighlightZone(e) {
+        const target = e.target;
+        const gridCellSize = map.getBoundingClientRect().width / 50;
+        const highlightZone = document.createElement('div');
+    
+        highlightZone.style.position = 'absolute';
+        highlightZone.style.width = `${8 * gridCellSize}px`;
+        highlightZone.style.height = `${8 * gridCellSize}px`;
+        highlightZone.style.backgroundColor = 'rgba(255, 0, 0, 0.4)';
+        highlightZone.style.pointerEvents = 'none';
+        highlightZone.style.zIndex = '9999';
+        highlightZone.id = 'highlight-zone';
+    
+        const left = parseFloat(target.style.left);
+        const top = parseFloat(target.style.top);
+    
+        highlightZone.style.left = `${left - ((8 - parseFloat(target.dataset.width)) / 2) * gridCellSize}px`;
+        highlightZone.style.top = `${top - ((8 - parseFloat(target.dataset.height)) / 2) * gridCellSize}px`;
+    
+        document.getElementById('overlay').appendChild(highlightZone);
+    }
+    
+    function remove8xRHighlightZone() {
+        const highlightZone = document.getElementById('highlight-zone');
+        if (highlightZone) {
+            highlightZone.remove();
+        }
+    }
+    
+    function show10x12HighlightZone(e) {
+        const target = e.target;
+        const gridCellSize = map.getBoundingClientRect().width / 50;
+        const highlightZone = document.createElement('div');
+    
+        highlightZone.style.position = 'absolute';
+        highlightZone.style.width = `${10 * gridCellSize}px`;
+        highlightZone.style.height = `${12 * gridCellSize}px`;
+        highlightZone.style.backgroundColor = 'rgba(0, 255, 0, 0.4)';
+        highlightZone.style.pointerEvents = 'none';
+        highlightZone.style.zIndex = '9999';
+        highlightZone.id = 'highlight-zone';
+    
+        const left = parseFloat(target.style.left);
+        const top = parseFloat(target.style.top);
+    
+        highlightZone.style.left = `${left - ((10 - parseFloat(target.dataset.width)) / 2) * gridCellSize}px`;
+        highlightZone.style.top = `${top - ((12 - parseFloat(target.dataset.height)) / 2) * gridCellSize}px`;
+    
+        document.getElementById('overlay').appendChild(highlightZone);
+    }
+    
+    function remove10x12HighlightZone() {
+        const highlightZone = document.getElementById('highlight-zone');
+        if (highlightZone) {
+            highlightZone.remove();
+        }
+    }
+    
+    function show6x8HighlightZone(e) {
+        const target = e.target;
+        const gridCellSize = map.getBoundingClientRect().width / 50;
+        const highlightZone = document.createElement('div');
+    
+        highlightZone.style.position = 'absolute';
+        highlightZone.style.width = `${6 * gridCellSize}px`;
+        highlightZone.style.height = `${8 * gridCellSize}px`;
+        highlightZone.style.backgroundColor = 'rgba(0, 255, 0, 0.4)';
+        highlightZone.style.pointerEvents = 'none';
+        highlightZone.style.zIndex = '9999';
+        highlightZone.id = 'highlight-zone';
+    
+        const left = parseFloat(target.style.left);
+        const top = parseFloat(target.style.top);
+    
+        highlightZone.style.left = `${left - ((6 - parseFloat(target.dataset.width)) / 2) * gridCellSize}px`;
+        highlightZone.style.top = `${top - ((8 - parseFloat(target.dataset.height)) / 2) * gridCellSize}px`;
+    
+        document.getElementById('overlay').appendChild(highlightZone);
+    }
+    
+    function remove6x8HighlightZone() {
+        const highlightZone = document.getElementById('highlight-zone');
+        if (highlightZone) {
+            highlightZone.remove();
+        }
+    }
+
+    function show10xHighlightZone(e) {
+        const target = e.target;
+        const gridCellSize = map.getBoundingClientRect().width / 50;
+        const highlightZone = document.createElement('div');
+    
+        highlightZone.style.position = 'absolute';
+        highlightZone.style.width = `${10 * gridCellSize}px`;
+        highlightZone.style.height = `${10 * gridCellSize}px`;
+        highlightZone.style.backgroundColor = 'rgba(255, 0, 0, 0.4)';
+        highlightZone.style.pointerEvents = 'none';
+        highlightZone.style.zIndex = '9999';
+        highlightZone.id = 'highlight-zone';
+    
+        const left = parseFloat(target.style.left);
+        const top = parseFloat(target.style.top);
+    
+        highlightZone.style.left = `${left - ((10 - parseFloat(target.dataset.width)) / 2) * gridCellSize}px`;
+        highlightZone.style.top = `${top - ((10 - parseFloat(target.dataset.height)) / 2) * gridCellSize}px`;
+    
+        document.getElementById('overlay').appendChild(highlightZone);
+    }
+    
+    function remove10xHighlightZone() {
+        const highlightZone = document.getElementById('highlight-zone');
+        if (highlightZone) {
+            highlightZone.remove();
+        }
+    }
+
+    function show16xHighlightZone(e) {
+        const target = e.target;
+        const gridCellSize = map.getBoundingClientRect().width / 50;
+        const highlightZone = document.createElement('div');
+    
+        highlightZone.style.position = 'absolute';
+        highlightZone.style.width = `${16 * gridCellSize}px`;
+        highlightZone.style.height = `${16 * gridCellSize}px`;
+        highlightZone.style.backgroundColor = 'rgba(255, 0, 0, 0.4)';
+        highlightZone.style.pointerEvents = 'none';
+        highlightZone.style.zIndex = '9999';
+        highlightZone.id = 'highlight-zone';
+    
+        const left = parseFloat(target.style.left);
+        const top = parseFloat(target.style.top);
+    
+        highlightZone.style.left = `${left - ((16 - parseFloat(target.dataset.width)) / 2) * gridCellSize}px`;
+        highlightZone.style.top = `${top - ((16 - parseFloat(target.dataset.height)) / 2) * gridCellSize}px`;
+    
+        document.getElementById('overlay').appendChild(highlightZone);
+    }
+    
+    function remove16xHighlightZone() {
+        const highlightZone = document.getElementById('highlight-zone');
+        if (highlightZone) {
+            highlightZone.remove();
+        }
+    }
+    
 });
